8263735735a43b2287dafbc7fd3efe6a0121dce7
aduc6z <anhduc6z@gmail.com>
2013-12-02 16:56:13 +0700

    Use job to execute task in background.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handler/HelloHandlers.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handler/HelloHandlers.java
@@ -3,15 +3,38 @@ package com.packtpub.e4.clock.ui.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 
 public class HelloHandlers extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		MessageDialog.openInformation(null, "Hello from handler class", "World of handler class");
+		Job job = new Job("About to say hello") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					
+				}
+//				MessageDialog.openInformation(null, "Hello", "World");
+				Display.getDefault().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						MessageDialog.openInformation(null, "Hello", "World");
+					}
+				});
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
 		return null;
 	}
-	
 
 }
