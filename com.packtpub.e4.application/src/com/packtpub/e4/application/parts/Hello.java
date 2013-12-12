1f60c679b33db91bbb8d22d9d6793ff988d7af34
aduc6z <anhduc6z@gmail.com>
2013-12-12 12:39:49 +0700

    Inject selection service to communicate between parts. When a part has selection event, selected object are sent to selection service which is available to other parts which register for selection event

--- com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
@@ -2,9 +2,12 @@ package com.packtpub.e4.application.parts;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -18,10 +21,22 @@ public class Hello {
 	@Optional // This service can be null and must be guarded by null check
 	private LogService log;
 	
+	@Inject
+	private MWindow window;
+	
+
+	@Inject
+	@Optional
+	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object selection) {
+		if (selection != null) {
+			label.setText(selection.toString());
+		}
+	}
+	
 	@PostConstruct
 	public void create(Composite parent) {
 		label = new Label(parent, SWT.NONE);
-		label.setText("Hello");
+		label.setText(window.getLabel());
 		if (log != null) {
 			log.log(LogService.LOG_ERROR, "Hello log");
 		}
