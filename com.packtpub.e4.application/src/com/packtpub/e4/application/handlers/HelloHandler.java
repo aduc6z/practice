27581f79af22a537a0f79539257d3d6a534e9922
aduc6z <anhduc6z@gmail.com>
2013-12-13 19:18:15 +0700

    Add handler to menuitem

--- com.packtpub.e4.application/src/com/packtpub/e4/application/handlers/HelloHandler.java
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/handlers/HelloHandler.java
@@ -1,11 +1,16 @@
 package com.packtpub.e4.application.handlers;
 
+import javax.inject.Named;
+
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class HelloHandler {
 
 	@Execute
-	public void hello() {
-		System.out.println("Hello World");
+	public void hello(@Named(IServiceConstants.ACTIVE_SHELL) Shell s) {
+		MessageDialog.openInformation(s, "Hello World", "Welcome to Eclipse 4 technology");
 	}
 }
