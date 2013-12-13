afb1de41a86de12817b5ca7aecb80a1020f3faea
aduc6z <anhduc6z@gmail.com>
2013-12-13 17:36:15 +0700

    Inject UISynchronize instance to execute tasks need to be performed in UI thread

--- com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
+++ com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
@@ -4,14 +4,22 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.osgi.service.log.LogService;
@@ -46,11 +54,47 @@ public class Hello {
 		prefs.sync();
 	}
 	
+	private Button button;
+	
+	
 	@Inject	@Named("math.random")
 	private Object random;
 	
+
+	@Inject
+	private UISynchronize ui;
+	
 	@PostConstruct
 	public void create(Composite parent) {
+		button = new Button(parent, SWT.PUSH);
+		button.setText("Do not push");
+		button.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				button.setEnabled(false);				
+				new Job("Button Pusher") {
+
+					@Override
+					protected IStatus run(IProgressMonitor monitor) {
+						ui.asyncExec(new Runnable() {
+
+							@Override
+							public void run() {
+								button.setEnabled(true);								
+							}
+							
+						});
+						return Status.OK_STATUS;						
+					}					
+				}.schedule(1000);
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				
+			}
+		});
 		label = new Label(parent, SWT.NONE);
 		label.setText("--" + greeting + " " + window.getLabel() + " " + random);
 		if (log != null) {
