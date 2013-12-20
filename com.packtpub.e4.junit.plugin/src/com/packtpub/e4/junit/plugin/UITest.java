bd9654fed484cfb95af7c634580fde27f5c989d0
aduc6z <anhduc6z@gmail.com>
2013-12-20 12:53:16 +0700

    Add code to get widget content using UIThread

--- com.packtpub.e4.junit.plugin/src/com/packtpub/e4/junit/plugin/UITest.java
+++ com.packtpub.e4.junit.plugin/src/com/packtpub/e4/junit/plugin/UITest.java
@@ -11,7 +11,9 @@ import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.results.StringResult;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -56,8 +58,17 @@ public class UITest {
 		assertNotNull(timeZoneView);
 		Widget widget = timeZoneView.getWidget();
 		org.hamcrest.Matcher<CTabItem> matcher = WidgetMatcherFactory.widgetOfType(CTabItem.class);
-		List<? extends CTabItem> ctabs = bot.widgets(matcher, widget);
+		final List<? extends CTabItem> ctabs = bot.widgets(matcher, widget);
 		assertEquals(18, ctabs.size());
+		
+		String tabText = UIThreadRunnable.syncExec(new StringResult() { // Need to use UIThread to access widget components
+			
+			@Override
+			public String run() {
+				return ctabs.get(0).getText();
+			}
+		});
+		assertEquals("Africa", tabText);
 	}
 	
 	
